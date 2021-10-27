bd1e6994a12d53724ec2e0d7607d689f4e889e48
Lori White <lori.r.white.24@gmail.com>
2021-10-27 12:39:25 -0400

    Updated basic_math.py

--- basic_math.py
+++ basic_math.py
@@ -5,3 +5,17 @@ print(myVar)
 myVar = 1
 myVar += 2
 print(myVar)
+# Basic Math Practice
+x = 5
+y = 8
+z = x + y
+print(x + y)
+print(z)
+print(f"{x} + {y} =", z)
+# Adding ints and floats
+j = 6
+k = 10.9
+print(j + k)
+print(int(j + k)) # This is flooring
+# Using BODMAS
+print((x - 5) + y ** 2)
