4386e2fa68a007592a255fcae9e3d52ed9998d13
Lori White <lori.r.white.24@gmail.com>
2021-10-27 15:03:31 -0400

    Created categorize_values.py

---
+++ categorize_values.py
@@ -0,0 +1,7 @@
+# Mixed Bag
+myMixedBagList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+secList = [4, 8, False]
+for item in myMixedBagList:
+    print("{} is of data type {}".format(item, type(item)))
+for value in secList:
+    print("{} is of data type {}".format(value, type(value)))
