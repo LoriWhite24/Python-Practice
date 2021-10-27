3836b68563bed3e9097382a1a0bb0be6e2449a45
Lori White <lori.r.white.24@gmail.com>
2021-10-27 11:54:04 -0400

    Created data_types.py and basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,7 @@
+# Assignment Operators
+myVar = 1
+myVar = myVar + 2
+print(myVar)
+myVar = 1
+myVar += 2
+print(myVar)
---
+++ data_types.py
@@ -0,0 +1,27 @@
+"""
+ Notes on Data Types in Python
+"""
+var = 1
+print(var)
+Var = 2.0 
+# Concatenating Strings and Numerics
+print("This is what Var is holding: " + str(Var))
+# Type casting
+print(float(var))
+print(int(Var))
+print(type(var))
+# The Numeric Data Types
+print("2 + 2 =", 2 + 2)
+print("4 - 2 =", 4 - 2)
+print("2 * 2 =", 2 * 2)
+print("4 / 2 =", 4 / 2)
+print("4 % 3 =", 4 % 3)
+# Data and thier Types
+myVar = 1
+print(myVar, "is of data type", type(myVar))
+myVar = 3.14
+print(myVar, "is of data type", type(myVar))
+myVar = 5j
+print(myVar, "is of data type", type(myVar))
+myVar = True
+print(myVar, "is of data type", type(myVar))
