a119f788c5de4aaef76fce893a7208341b7b759b
Lori White <lori.r.white.24@gmail.com>
2021-11-02 18:39:59 -0400

    Used pylint to check code styling

--- Debugging_Practice/dynamic_debug.py
+++ Debugging_Practice/dynamic_debug.py
@@ -2,9 +2,11 @@
     Author: Lori White
     Purpose: Showing how to use Dynamic Debugging in python.
 """
-# Ask the user for a value and confirm the supplied value is greater than 0
 def checkvalue(valuetocheck):
-    assert (type(valuetocheck) is int or type(valuetocheck) is float), "You must enter a number."
+    """
+     Ask the user for a value and confirm the supplied value is greater than 0
+    """
+    assert (isinstance(valuetocheck,(int,float))), "You must enter a number."
     assert (valuetocheck > 0), "Value entered must be greater then 0."
     if valuetocheck > 4:
         print("Value is greater than 4.")
--- Debugging_Practice/static_debug.py
+++ Debugging_Practice/static_debug.py
@@ -3,9 +3,14 @@
     Purpose: Showing how to use Static Debugging in python.
 """
 # Python program with 2 errors
-var = "Double Value"
-sumValue = var + 4 # Expected Output: TypeError: can only concatenate str (not "int") to str
+VAR = "Double Value"
+sumValue = VAR + 4 # Expected Output: TypeError: can only concatenate str (not "int") to str
 
 def dosomething(valuetocheck):
+    """
+     Checks a value.
+    """
     if valuetocheck > 4:
-        print("Bad indent") # Try playing with the indention and see what happens! Static Analysis at work!
+        print("Bad indent")
+        # Try playing with the indention and see what happens!
+        # Static Analysis at work!
